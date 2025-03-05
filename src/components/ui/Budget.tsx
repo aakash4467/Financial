b0de1d4cb76b55840bfa58be81cd7fe5370a8bb2
aakash4467 <khadkaaakash166@gmail.com>
2025-03-05 15:17:58 +0545

    Updating code for chart

--- src/components/ui/Budget.tsx
+++ src/components/ui/Budget.tsx
@@ -1,12 +1,14 @@
 import { View, Text } from "react-native";
+import Chart from "./Chart";
 
 export default function Budget() {
   return (
-    <View>
+    <View className="gap-5">
       <View className="flex-row gap-x-[150px] items-center">
         <Text className="text-[20px] font-semibold">Budgeting</Text>
         <Text className="text-[15px] font-semibold text-[#928383]">View Analytics</Text>
       </View>
+      <Chart />
     </View>
   );
 }
